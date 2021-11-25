00105dc1b2a918d2fcaa2902652170f2dc8cd35f
Emily Hudson <emily33901@gmail.com>
2021-11-25 14:44:40 +0000

    format time as float

--- backend/src/ui/controls.rs
+++ backend/src/ui/controls.rs
@@ -16,14 +16,14 @@ impl ControlsElement {
         Row::new()
             .push(Button::new(&mut self.play_button, Text::new("play")).on_press(Message::Resume))
             .push(Button::new(&mut self.pause_button, Text::new("pause")).on_press(Message::Pause))
-            .push(Text::new(format!("{:?}", location)))
+            .push(Text::new(format!("{:.1}", location.as_secs_f32())))
             .push(Slider::new(
                 &mut self.slider,
                 RangeInclusive::new(0.0, total.as_secs_f64()),
                 location.as_secs_f64(),
                 |_| Message::None,
             ))
-            .push(Text::new(format!("{:?}", total)))
+            .push(Text::new(format!("{:.1}", total.as_secs_f32())))
             .align_items(iced::Align::Center)
     }
 }
