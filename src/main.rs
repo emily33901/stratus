cf21832adf0d1e44a7c8d33b23243ed9376a6bfb
Emily Hudson <emily33901@gmail.com>
2022-05-28 23:37:37 +0100

    Remove old code

--- src/main.rs
+++ src/main.rs
@@ -21,27 +21,6 @@ fn main() -> Result<()> {
         .chain(fern::log_file("output.log")?)
         .apply()?;
 
-    #[cfg(target_os = "")]
-    {
-        let sc = sc::new()?;
-
-        let song = sc
-            .song(Id::Url("https://soundcloud.com/iammindsight/anotherone"))
-            .await?;
-
-        let user = sc.user(Id::Url("https://soundcloud.com/f1ssi0n")).await?;
-
-        println!("{:#?}", song.read());
-        println!("{:#?}", user.read());
-
-        let playlist = sc
-            .playlist(Id::Url("https://soundcloud.com/f1ssi0n/sets/june-1"))
-            .await?;
-
-        for song in &playlist.read().songs {
-            println!("{:#?}", song);
-        }
-    }
     let options: iced::Settings<()> = iced::Settings {
         ..Default::default()
     };
